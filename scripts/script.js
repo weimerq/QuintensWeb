b91c4280ac77e8f6d3f80e9c540e3a9bdc3330bb
weimerq <150670107+weimerq@users.noreply.github.com>
2023-12-10 13:45:49 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,7 @@
-// JavaScript Document
-console.log("hi");
+var logo = document.querySelector('.hamburger');
+var menu = document.querySelector('.menu');
+
+logo.addEventListener('click',function(){
+    menu.classList.toggle("showmenu");
+})
+
